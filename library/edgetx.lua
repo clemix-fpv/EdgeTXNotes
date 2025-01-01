03999cca6f8297f521abfd87f030862c03525f9f
Clemens Famulla-Conrad <devel@clemix.de>
2025-01-01 21:33:45 +0100

    Start with lua lib to get autocompletion
    Needed to add the ./library folder to my lua_ls (lsp-server) config.
    
    ```
    lspconfig.lua_ls.setup({
        on_attach = nvlsp.on_attach,
        capabilities = nvlsp.capabilities,
        settings = {
          Lua = {
            -- runtime = {
            --   -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
            --   version = "LuaJIT",
            --   path = vim.split(package.path, ";"),
            -- },
            -- diagnostics = {
            --   -- Get the language server to recognize the `vim` global
            --   globals = { "vim" },
            -- },
            workspace = {
              -- Make the server aware of Neovim runtime files and plugins
              library = { vim.env.VIMRUNTIME, "./library"  },
              checkThirdParty = false,
            },
            telemetry = {
              enable = false,
            },
          },
        },
      })
    ```

---
+++ library/edgetx.lua
@@ -0,0 +1,26 @@
+
+---@meta
+
+---@class Character
+lcd = {}
+
+---@param text string
+function lcd.sizeText(text)
+    return string.len(text), 1
+end
+
+---@param path string
+function dir(path)
+end
+
+---@param path string
+function fstat(path)
+end
+
+---@class bit32
+---
+
+bit32 = {}
+
+function bit32.band(value, mask1, ...)
+end
